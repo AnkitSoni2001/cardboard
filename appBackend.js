c65b46c3730bf231d1ac59f6c0ff4227f7b6ac0b
AnkitSoni2001 <ankitsoni221010@gmail.com>
2022-02-24 22:09:23 +0530

    Initial commit

---
+++ appBackend.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const socket = require("socket.io");
+
+
+
+const app = express(); //Initialized and server ready
+
+app.use(express.static("public"));
+
+
+
+//creating port
+let port = 5000;
+let server = app.listen(port, () => {
+    console.log("Listening to port" + port);
+})
+
+// connecting with server
+
+let io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+
+    //Received data from frontend
+    socket.on("beginPath", (data) => {
+        // Now server transfer same data to all connected computer
+        io.sockets.emit("beginPath", data);
+    })
+
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+    socket.on("redoUndo", (data) => {
+        io.sockets.emit("redoUndo", data);
+    })
+})
